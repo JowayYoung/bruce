4fec266cc09a6862ba08506c8647a02094e89894
JowayYoung <young.joway@aliyun.com>
2021-04-07 18:01:47 +0800

    修复路径

--- packages/bruce-cli/packages/action-locale/index.js
+++ packages/bruce-cli/packages/action-locale/index.js
@@ -7,9 +7,8 @@ import { AbsPath, ShowMsg, ShowTitle } from "../../utils/setting.js";
 export default async function() {
 	ShowTitle("locale");
 	const LOCALE = await LocaleAnswer();
-	const i18nP = AbsPath("../i18n/index.js", 1);
+	const i18nP = AbsPath("../languages/index.js", 1);
 	const i18nSC = readFileSync(i18nP, "utf8");
-	console.log(i18nP);
 	const i18nDC = i18nSC.replace(/"(.+?)"/, `"./${LOCALE}.js"`);
 	writeFileSync(i18nP, i18nDC, "utf8");
 	ShowMsg("green", LOCALE_TEXT.localed);
